220ec1c40e436d3450e5fe30f2ed03f5e4b6795a
Ben Do <bdo.appworkshop@gmail.com>
2022-11-13 20:57:24 -0600

    Imported useContext, UserContext, and signOutUser.

--- src/components/routes/navigation/navigation.component.jsx
+++ src/components/routes/navigation/navigation.component.jsx
@@ -1,9 +1,19 @@
 import { Outlet, Link } from 'react-router-dom';
-import { Fragment } from 'react';
+import { Fragment, useContext } from 'react';
+
 import { ReactComponent as CrwnLogo } from '../../../assets/crown.svg';
 import './navigation.styles.scss';
+import {UserContext} from '../../../contexts/user.context';
+
+import {signOutUser} from '../../../utils/firebase/firebase.utils';
 
 const Navigation = () => {
+  const {currentUser} = useContext(UserContext);
+  // console.log(currentUser);
+
+  const signOutHandler = async () => {
+    await signOutUser();
+  }
 
   return (
     <Fragment>
@@ -15,9 +25,10 @@ const Navigation = () => {
           <Link className='nav-link' to='/shop'>
             SHOP
           </Link>
-          <Link className='nav-link' to='/auth'>
+          {currentUser ? (<span className='nav-link' onClick={signOutHandler}>SIGN OUT</span>) : (<Link className='nav-link' to='/auth'>
             SIGN IN
-          </Link>
+          </Link>)}
+
         </div>
       </div>
       <Outlet />
